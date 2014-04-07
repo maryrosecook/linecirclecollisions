81fa781f777c940363aaf9399ff08edcb435ef44
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-04-07 15:45:16 -0400

    first

---
+++ main.js
@@ -0,0 +1,235 @@
+;(function(exports) {
+  var SCREEN_SIZE = { x: 480, y: 480 };
+
+  // sets up lines and circles and starts the animation
+  function start() {
+    // screen to draw on
+    var screen = document.getElementById("canvas").getContext('2d');
+
+    // make new circle every so often
+    var circles = [];
+    setInterval(function() {
+      circles.push(makeCircle());
+    }, 400);
+
+    // make grid of lines
+    var lines = [];
+    for (var i = 1; i < 6; i++) {
+      for (var j = 1; j < 6; j++) {
+        lines.push(makeLine(i * 80, j * 80, i * j));
+      }
+    }
+    lines.splice(10, 2); // throw away top center lines
+
+    // start update/draw loop
+    function tick() {
+      update(circles, lines);
+      draw(circles, lines, screen);
+      requestAnimationFrame(tick);
+    };
+
+    tick();
+  };
+  exports.start = start; // make start function available to HTML page
+
+  // rotates the lines, moves and bounces the circles
+  function update(circles, lines) {
+    for (var i = circles.length - 1; i >= 0; i--) {
+      for (var j = 0; j < lines.length; j++) {
+        bounceCircle(circles[i], lines[j]);
+      }
+
+      moveCircle(circles[i]);
+      if (!isCircleOnScreen(circles[i])) {
+        circles.splice(i, 1); // remove off-screen circle
+      }
+    }
+
+    for (var i = 0; i < lines.length; i++) {
+      lines[i].angle += lines[i].rotateSpeed;
+    }
+  };
+
+  function draw(circles, lines, screen) {
+    // fill screen with white
+    screen.fillStyle = "white";
+    screen.fillRect(0, 0, SCREEN_SIZE.x, SCREEN_SIZE.y);
+
+    // draw circles
+    for (var i = 0; i < circles.length; i++) {
+      drawCircle(circles[i], screen);
+    }
+
+    // draw lines
+    for (var i = 0; i < lines.length; i++) {
+      drawLine(screen, lines[i]);
+    }
+  };
+
+  function makeCircle() {
+    var radius = 7;
+    return {
+      center: {
+        x: SCREEN_SIZE.x / 2, // center x coordinate
+        y: -radius + 1, // center y coordinate
+      },
+      velocity: { x: 0, y: 0 },
+      radius: radius,
+    };
+  };
+
+  function makeLine(x, y) {
+    return {
+      center: { x: x, y: y },
+      span: 40,
+      angle: Math.random() * 360,
+      rotateSpeed: 0.5
+    };
+  };
+
+  function distance(point1, point2) {
+    var x = Math.abs(point1.x - point2.x);
+    var y = Math.abs(point1.y - point2.y);
+    return Math.sqrt((x * x) + (y * y));
+  };
+
+  function magnitude(vector) {
+    return Math.sqrt(vector.x * vector.x + vector.y* vector.y);
+  };
+
+  function unitVector(vector) {
+    return {
+      x: vector.x / magnitude(vector),
+      y: vector.y / magnitude(vector)
+    };
+  };
+
+  function dotProduct(vector1, vector2) {
+    return vector1.x * vector2.x + vector1.y * vector2.y;
+  };
+
+  // returns the points at the two ends of the passed line
+  function lineEnds(line) {
+    var angleRadians = line.angle * 0.01745;
+    var lineVector = unitVector({
+      x: Math.cos(angleRadians) * 0 - Math.sin(angleRadians) * -1,
+      y: Math.sin(angleRadians) * 0 + Math.cos(angleRadians) * -1
+    });
+
+    return [{
+      x: line.center.x + lineVector.x * line.span / 2,
+      y: line.center.y + lineVector.y * line.span / 2
+    }, {
+      x: line.center.x - lineVector.x * line.span / 2,
+      y: line.center.y - lineVector.y * line.span / 2
+    }];
+  };
+
+  function drawLine(screen, line) {
+    var end1 = lineEnds(line)[0];
+    var end2 = lineEnds(line)[1];
+
+    screen.beginPath();
+    screen.lineWidth = 2;
+    screen.moveTo(end1.x, end1.y);
+    screen.lineTo(end2.x, end2.y);
+    screen.closePath();
+
+    screen.strokeStyle = "black";
+    screen.stroke();
+  };
+
+  function drawCircle(circle, screen) {
+    screen.beginPath();
+    screen.arc(circle.center.x, circle.center.y, circle.radius, 0, Math.PI * 2, true);
+    screen.closePath();
+    screen.fillStyle = "black";
+    screen.fill();
+  };
+
+  // returns point on passed line closest to passed circle
+  function pointOnLineClosestToCircle(circle, line) {
+    var lineEnd1 = lineEnds(line)[0];
+    var lineEnd2 = lineEnds(line)[1];
+
+    // vector representing line surface
+    var vectorOfLine = {
+      x: lineEnd2.x - lineEnd1.x,
+      y: lineEnd2.y - lineEnd1.y
+    };
+    var unitLine = unitVector(vectorOfLine);
+
+    var lineEnd1ToCircle = {
+      x: circle.center.x - lineEnd1.x,
+      y: circle.center.y - lineEnd1.y
+    };
+
+    // project vector between line end and circle along
+    // line to get distance between end and point
+    // on line closest to circle
+    var projection = dotProduct(lineEnd1ToCircle, unitLine);
+
+    if (projection <= 0) {
+      return lineEnd1; // off end of line - end is closest point
+    } else if (projection >= magnitude(vectorOfLine)) {
+      return lineEnd2; // ditto
+    } else {
+      // part way along line - return that point
+      return {
+        x: lineEnd1.x + unitLine.x * projection,
+        y: lineEnd1.y + unitLine.y * projection
+      };
+    }
+  };
+
+  function isCircleIntersectingLine(circle, line) {
+    var closest = pointOnLineClosestToCircle(circle, line);
+    var circleToLineDistance = distance(circle.center, closest);
+    return circleToLineDistance < circle.radius;
+  };
+
+  // bounces circle off line
+  function bounceCircle(circle, line) {
+    if (!isCircleIntersectingLine(circle, line)) {
+      return; // line not touching circle - no bounce
+    }
+
+    // get normal of surface to bounce circle off
+    var closest = pointOnLineClosestToCircle(circle, line);
+    var normal = unitVector({
+      x: circle.center.x - closest.x,
+      y: circle.center.y - closest.y
+    });
+
+    // set new circle velocity by reflecting old velocity in
+    // the normal to the surface the circle is bouncing off
+    var dot = dotProduct(circle.velocity, normal);
+    circle.velocity.x = circle.velocity.x - 2 * dot * normal.x;
+    circle.velocity.y = circle.velocity.y - 2 * dot * normal.y;
+
+    // move circle until outside line
+    while (isCircleIntersectingLine(circle, line)) {
+      moveCircle(circle);
+    }
+  };
+
+  // move passed circle based on its current speed
+  function moveCircle(circle) {
+    circle.velocity.y = circle.velocity.y + 2;
+    circle.center.x = circle.center.x + circle.velocity.x / 30;
+    circle.center.y = circle.center.y + circle.velocity.y / 30;
+  };
+
+  function isCircleOnScreen(circle) {
+    return circle.center.x > -circle.radius &&
+      circle.center.x < SCREEN_SIZE.x + circle.radius &&
+      circle.center.y > -circle.radius &&
+      circle.center.y < SCREEN_SIZE.y + circle.radius;
+  };
+})(this);
+
+
+// concepts will learn:
+// - detecting line circle collisions
+// - line normals
+// - bouncing off arbitrarily angled surfaces
